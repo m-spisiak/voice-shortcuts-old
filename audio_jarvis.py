c0be4692dc0bff70207cf7d2494b1ffe2cfb74d9
star1master <82773579+star1master@users.noreply.github.com>
2021-04-22 22:21:57 +0200

    Add files via upload

---
+++ audio_jarvis.py
@@ -0,0 +1,58 @@
+import speech_recognition as sr
+import time
+import pyautogui as pag
+
+r = sr.Recognizer()
+
+keywords = [("copy", 1), ("print", 1), ("snap",1), ("select all",1) ]
+
+source = sr.Microphone(device_index=1)
+
+
+def callback(recognizer, audio):
+
+    try:
+        speech = recognizer.recognize_sphinx(audio, keyword_entries=keywords)
+        if "copy" in speech:
+            copy()
+        elif "paste" in speech:
+            paste()
+        elif "snap" in speech:
+            snap()
+        elif "select all" in speech:
+            select_all()
+
+    except sr.UnknownValueError:
+        print("Oops! Didn't catch that")
+
+
+def copy():
+    print("copying")
+    pag.keyDown("ctrl")
+    pag.press("c")
+    pag.keyUp("ctrl")
+
+def paste():
+    print("pasting")
+    pag.keyDown("ctrl")
+    pag.press("v")
+    pag.keyUp("ctrl")
+
+def snap():
+    print("cutting")
+    pag.keyDown("ctrl")
+    pag.press("x")
+    pag.keyUp("ctrl")
+
+def select_all():
+    print("selcting all")
+    pag.keyDown("ctrl")
+    pag.press("a")
+    pag.keyUp("ctrl")
+
+def start_recognizer():
+    r.listen_in_background(source, callback)
+    time.sleep(1000000)
+    
+
+start_recognizer()
